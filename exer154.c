a862bf5eb41879ae218b18b4690a0394f62d77b9
Jethvaharesh <jethvaharesh93@gmail.com>
2023-09-17 16:06:50 +0530

    ok

---
+++ exer154.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{   printf(" \t*********  find squar of number=  ");
+    int number;
+    //number=25;
+    scanf("%d",&number);
+    printf("\n\t\t    squar of %d = %d",number,(number*number));
+
+
+}
