b2b4ee497c14249326e30cf2427a02a9edd4cb94
Jethvaharesh <jethvaharesh93@gmail.com>
2023-09-17 13:45:12 +0530

    a.exe

---
+++ exer15.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+     int num;
+     //num=9
+     
+    printf("\t\tEnter value of num :  ");  
+    scanf("%d",&num);
+    printf("\t********* Multiplicatin of number %d **************\n\n",num);
+    printf("  \t\t %d x 1 = %d \n",num,(num*1));
+    printf("  \t\t %d x 2 = %d \n",num,(num*2));
+    printf("  \t\t %d x 3 = %d \n",num,(num*3));
+    printf("  \t\t %d x 4 = %d \n",num,(num*4));
+    printf("  \t\t %d x 5 = %d \n",num,(num*5));
+    printf("  \t\t %d x 6 = %d \n",num,(num*6));
+    printf("  \t\t %d x 7 = %d \n",num,(num*7));
+    printf("  \t\t %d x 8 = %d \n",num,(num*8));
+    printf("  \t\t %d x 9 = %d \n",num,(num*9));
+    printf("  \t\t %d x 10= %d \n",num,(num*10));
+
+}
