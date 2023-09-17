e12be326e8c0d1c6286afcb732e9436ec11f79b4
Jethvaharesh <jethvaharesh93@gmail.com>
2023-09-17 15:06:17 +0530

    ok

---
+++ exer153.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int lenth;
+  //lenth=20;
+  printf("\t\tlenth is ");
+  scanf("%d",&lenth);
+  int cube;
+  cube=lenth*lenth*lenth;
+  printf(" \n\t\tvolume of cube is %d",cube);
+  
+
+
+
+}
