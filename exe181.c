f64f8cb66a1e881a9e2973cec9a3f118d2258343
Jethvaharesh <jethvaharesh93@gmail.com>
2023-09-18 21:41:54 +0530

    ok

---
+++ exe181.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+   printf("\t\twrite program to check number1 small.\n\n");
+ //write a program to find income taxe income :5 lk taxe rate : 5% .
+   int number1,number2;
+   printf("\n\t\tENTER VALUE OF NUMBER 1  ");
+   scanf("%d",&number1);
+   printf("\n\t\tENTER VALUE OF NUMBER 2  ");
+   scanf("%d",&number2);
+
+   if(number1<number2)
+   {
+     printf("\n\t\tnumber 1 is smaller than number 2 \n");
+
+     printf("\t\tgood by");
+
+   }
+
+
+
+
+
+
+
+
+}
