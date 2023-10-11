cffa51e726e678dd09301a883ef3b1c9c27889ec
Jethvaharesh <jethvaharesh93@gmail.com>
2023-10-11 08:35:56 +0530

    ok

--- exdowhile.c
+++ exdowhile.c
@@ -1,66 +1,74 @@
-//write area of triangle,circle,cylinder,sqareand also checke for continue and stop.
-#include<stdio.h>
+// write area of triangle,circle,cylinder,sqareand also checke for continue and stop.
+#include <stdio.h>
 void main()
 {
-    int number,operation,temp;
-    temp=200;
-    float height, base,area,pi,radius;
-    pi=3.141592;
-    
-    do
-    {
-       printf("\nselect number for operation of area \n");
-       printf("\n 1 for area of triangle \n");
-       printf("\n 2 for area of circle \n");
-       printf("\n 3 for area of cylinder \n");
-       printf("\n 4 for area of sqare \n");
-       printf("\n 5 for exit \n");
-       printf("\n enter number: ");
-       scanf("%d",&operation);
+   int number, operation, temp,sqare;
+   temp = 200;
+   float height, base, area, pi, radius;
+   pi = 3.141592;
 
-       switch(operation)
-       {
-         case 1:
+   do
+   {
+      printf("\n\nselect operation for area");
+      printf("\n 1 for area of triangle ");
+      printf("\n 2 for area of circle ");
+      printf("\n 3 for area of cylinder ");
+      printf("\n 4 for sqare of number ");
+      printf("\n 5 for exit ");
+      printf("\n enter number: ");
+      scanf("%d", &operation);
 
-         printf("\n area of triangle ((height*base)/2) \n");
-         printf("\n enter heghit is : " );
-         scanf("%f",&height);
+      switch (operation)
+      {
+      case 1:
+
+         printf("\n|||||| area of triangle ||||||| \n"); //((height*base)/2)
+         printf("\n enter heghit is : ");
+         scanf("%f", &height);
          printf("\n enter base is : ");
-         scanf("%f",&base);
-         area=(height*base)/2;
-         printf("\n\narea of triangle is %f",area);
+         scanf("%f", &base);
+         area = (height * base) / 2;
+         printf("\n\n|||||area of triangle is %f |||||", area);
          break;
 
-         case 2:
-           
-           printf("\n area of circle(pi(r*r)) \n");
-           printf("enter radius is: ");
-           scanf("%f",&radius);
-           area=pi*(radius*radius);
-           printf("\n\n area of circle is:%.2f",area);
-           break;
+      case 2:
 
+         printf("\n|||||area of circle||||| \n"); // (pi(r*r))
+         printf("\n\nenter radius is: ");
+         scanf("%f", &radius);
+         area = pi * (radius * radius);
+         printf("\n\n|||||| area of circle is:%.2f||||||", area);
+         break;
 
+      case 3:
 
+         printf("\n|||||area of cylinder||||| \n"); //(2pirh+(2pirr))
+         printf("\n\nenter radius is: ");
+         scanf("%f", &radius);
+         printf("\n enter height is:  ");
+         scanf("%f", &height);
+         area = 2 * pi * radius * height + (2 * pi * (radius * radius));
+         printf("\n\n|||||| area of cylinder is:%.2f||||||", area);
+         break;
 
+      case 4:
 
-        default:
-          printf("\n invalide number choose valide number\n");
-          break;
+         printf("\n|||||number of sqare||||| \n"); 
+         printf("\n\nenter number is: ");
+         scanf("%d", &number);
+         sqare=number * number;
+         printf("\n\n|||||| sqare of %d is: %d ||||||",number,sqare);
+         break;
 
+         case 5:
+         
+         temp=404;
+         break;
 
-       }
+      default:
+         printf("\n invalide number choose valide number\n");
+         break;
+      }
 
-
-
-
-
-
-    }while(temp!=404);
-
-
-
-
-
-
+   } while (temp != 404);
 }
